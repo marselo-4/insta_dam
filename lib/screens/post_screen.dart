d4c603b869e171efcb89a64b08ea6601d3c1a525
Carlos Martínez (marselo.exe) <78843770+marselo-4@users.noreply.github.com>
2024-10-29 20:20:18 +0100

    Refactor navigation and MainScaffold

--- lib/screens/post_screen.dart
+++ lib/screens/post_screen.dart
@@ -5,6 +5,6 @@ class PostScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Placeholder(child: Center(child:Text("Post"),));
   }
 }
