154f8fac582c73113593006cd2af174dd01fcd62
LujanIdn <148265994+LujanIdn@users.noreply.github.com>
2024-10-30 20:14:14 +0100

    LoginPage_fase1

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -6,18 +6,74 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white70,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset("assets/img/error.png"),
-                const Text("Eslogan"),
-                TextField(),
-                TextField(),
-                ElevatedButton(onPressed: () { Navigator.pushNamed(context, '/home');}, child: Text("Enter")),
-                ElevatedButton(onPressed: () { Navigator.pushNamed(context, '/signup');}, child: Text("Register"))
-              ],
+          children: [
+            Image.asset(
+              "assets/img/logo.png",
+              width: 300,
+              height: 200,
+            ),
+            Image.asset(
+              "assets/img/instadam.jpg",
+              width: 300,
+              height: 100,
+            ),
+            const Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30.0),
+              child: const Text(
+                'Conecta a través de imágenes',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 34,
+                  fontWeight: FontWeight.w900,
+                  fontFamily: 'roboto',
+                  letterSpacing: 1.2,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+           TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            textInputAction: TextInputAction.next,
+            cursorColor: Colors.black,
+            decoration: const InputDecoration(
+              hintText: "Your email",
+              prefixIcon: Padding(
+                padding: EdgeInsets.all(10),
+                child: Icon(Icons.person),
+              ),
+            ),
           ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: TextFormField(
+              textInputAction: TextInputAction.done,
+              obscureText: true,
+              cursorColor: Colors.black,
+              decoration: const InputDecoration(
+                hintText: "Your password",
+                prefixIcon: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Icon(Icons.lock),
+                ),
+              ),
+            ),
+          ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/home');
+                },
+                child: Text("Enter")),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/signup');
+                },
+                child: Text("Register"))
+          ],
+        ),
       ),
     );
   }
