2b3711dd967c7eada55d484ffca6db88e6f53377
LujanIdn <148265994+LujanIdn@users.noreply.github.com>
2024-10-30 16:32:25 +0100

    automaticallyImplyLeading --> true

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -26,7 +26,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('InstaDam'),
-        automaticallyImplyLeading: false,
+        automaticallyImplyLeading: true,
       ),
       body: PageView(
         controller: _pageController,
