c7465d26ebfb297c20771660286e04a56e42ba7c
bazoca333 <alvaromillanes33@gmail.com>
2024-10-30 20:13:08 +0100

    descripcion + botones

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,86 +1,171 @@
-import 'package:flutter/material.dart';
+  import 'dart:math';
 
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+  import 'package:flutter/material.dart';
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Column(
-          children: [
-            Center(
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                color: Colors.amber,
-                padding: const EdgeInsets.all(16.0),
-                child: const Text(
-                  'bazoca33',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+  class ProfileScreen extends StatefulWidget {
+
+    const ProfileScreen({super.key});
+
+    @override
+    State<ProfileScreen> createState() => _ProfileScreenState();
+  }
+
+  class _ProfileScreenState extends State<ProfileScreen> {
+    String avatarUrl = "https://picsum.photos/200";
+    String postNumber = Random().nextInt(10).toString();
+    String followersNumber = Random().nextInt(1000).toString();
+    String followingNumber = Random().nextInt(100).toString();
+    String accName = 'Alvaro Millanes Sabaté';
+  String descripcion = 'Viviendo una aventura a la vez 🌍✨\n'
+      'Aquí para compartir lo que amo y lo que hago ❤️\n'
+      'Amante de los pequeños detalles y las grandes experiencias 🌟';
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+          body: Column(
+            children: [
+              Container(
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.amber,
+                  padding: const EdgeInsets.all(16.0),
+                  child: const Text(
+                    'bazoca33',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
                   ),
-                  textAlign: TextAlign.center,
                 ),
+          
+          //foto + seguidores, seguidos...
+          Container(
+              width: MediaQuery.of(context).size.width,
+              height: 120,
+              padding: const EdgeInsets.only(left: 8, right: 8),
+              child:  Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Align(
+                    alignment: const FractionalOffset(.2, .5),
+                    child: CircleAvatar(backgroundImage: NetworkImage(avatarUrl),
+                    radius: 55,)
+                  ),
+                  
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(postNumber, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                      const Text("pubicaciones")
+                    ],
+                  ),
+
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(followersNumber, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                      const Text("seguidores")
+                    ],  
+                  ),
+
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(followingNumber, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                      const Text("siguiendo")
+                    ],
+                  )
+                ],
+              )
+
+              
+            ),
+          // descripcion
+          Center(
+            
+            child: Container(
+              padding: const EdgeInsets.only(left: 8, right: 8),
+              width: MediaQuery.of(context).size.width,
+              constraints: BoxConstraints(maxHeight: 150),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //Nombre persona
+                  Text(accName, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
+                
+                //descripcion
+                Text(descripcion, maxLines: 5, overflow: TextOverflow.ellipsis,)
+                ],
+                
               ),
             ),
-         
-        //foto + seguidores, seguidos...
-        Container(
-            width: MediaQuery.of(context).size.width,
-            height: 120,
-            color: Colors.blue,
-            padding: const EdgeInsets.only(left: 20, right: 20),
-            child:  Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Align(
-                  alignment: FractionalOffset(.2, .5),
-                  child: CircleAvatar(backgroundImage: NetworkImage("https://media.licdn.com/dms/image/v2/D5603AQHyYxdhP8mV2Q/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1719498858258?e=2147483647&v=beta&t=k2WoJyiwPQnDjvjr9wvnazb1eGIL519yW_sxjdujmtI"),
-                  radius: 55,)
-                ),
-                
-                Align(
-                  alignment: const FractionalOffset(.5, .5),
-                  child: ElevatedButton(onPressed: () {}, child: const Text("Seguir")),
-                ),
+          ),
+          //editar + compartir
+          Center(
+            child: Container(
+              padding: const EdgeInsets.only(left: 8, right: 8),
+              width: MediaQuery.of(context).size.width,
+              height: 75,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          side: const BorderSide(color: Colors.black, width: .5), 
+                        ),
+                        padding: const EdgeInsets.all(8), 
+                      ),
+                      child: const Text(
+                        'Editar',
+                        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black, fontSize: 16), 
+                      ),
+                    ),
+                  ),
 
-                Align(
-                  alignment: const FractionalOffset(.5, .5),
-                  child: ElevatedButton(onPressed: () {}, child: const Text("Mensaje")),
-                )
-              ],
-            )
+                  const SizedBox(width: 8), 
 
-            
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          side: const BorderSide(color: Colors.black, width: .5),
+                        ),
+                        padding: const EdgeInsets.all(8),
+                      ),
+                      child: const Text(
+                        'Compartir',
+                        style: TextStyle(color: Colors.black, fontSize: 16), 
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+            ),
           ),
-        // descripcion
-        Center(
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: 150,
-            color: Colors.purple,
+          //posts
+          Expanded(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              color: Colors.green,
+            ),
           ),
-        ),
-        //follow + mensaje
-        Center(
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: 75,
-            color: Colors.red,
-          ),
-        ),
-        //posts
-        Expanded(
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            color: Colors.green,
-          ),
-        ),
+          
+            ],
+          )
         
-          ],
-        )
-      
-    );
+      );
+    }
   }
-}
