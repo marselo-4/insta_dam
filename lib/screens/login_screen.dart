a8a68ad9f7e4618c3f808dac583b7006ed247484
LujanIdn <lujancastillo2015@alumnes.ilerna.com>
2024-10-31 12:18:55 +0100

    TextFields_Done

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,78 +1,104 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool visible = true;
+
+  @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      backgroundColor: Colors.white70,
+      backgroundColor: Colors.white,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              "assets/img/logo.png",
-              width: 300,
-              height: 200,
-            ),
-            Image.asset(
-              "assets/img/instadam.jpg",
-              width: 300,
-              height: 100,
-            ),
-            const Padding(
-              padding: const EdgeInsets.symmetric(vertical: 30.0),
-              child: const Text(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/img/logo.png",
+                width: 200,
+                height: 100,
+              ),
+              Image.asset(
+                "assets/img/instadam.jpg",
+                width: 300,
+                height: 100,
+              ),
+              const Text(
                 'Conecta a través de imágenes',
                 style: TextStyle(
                   color: Colors.black,
-                  fontSize: 34,
+                  fontSize: 24,
                   fontWeight: FontWeight.w900,
                   fontFamily: 'roboto',
                   letterSpacing: 1.2,
                 ),
                 textAlign: TextAlign.center,
               ),
-            ),
-           TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            textInputAction: TextInputAction.next,
-            cursorColor: Colors.black,
-            decoration: const InputDecoration(
-              hintText: "Your email",
-              prefixIcon: Padding(
-                padding: EdgeInsets.all(10),
-                child: Icon(Icons.person),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: TextFormField(
-              textInputAction: TextInputAction.done,
-              obscureText: true,
-              cursorColor: Colors.black,
-              decoration: const InputDecoration(
-                hintText: "Your password",
-                prefixIcon: Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Icon(Icons.lock),
+              SizedBox(height: 20,),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  labelStyle: TextStyle(color: Colors.black, fontSize: 15),
+                  filled: true,
+                  fillColor: Colors.black12,
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.black, width: 3)),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.pink, width: 3)),
+                  prefixIcon: Icon(Icons.person),
                 ),
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.next,
               ),
-            ),
+              SizedBox(height: 50),
+              TextField(
+                decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: TextStyle(color: Colors.black, fontSize: 15),
+                    filled: true,
+                    fillColor: Colors.black12,
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.black, width: 3)),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.pink, width: 3)),
+                    prefixIcon: Icon(Icons.lock_outline_rounded),
+                    suffixIcon: IconButton(
+                      icon: Icon(visible ? Icons.visibility_off : Icons.visibility),
+                      onPressed: () {
+                        setState(() {
+                        visible = !visible;
+                        });
+                      },
+                    )),
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.next,
+                obscureText: visible,
+              ),
+              
+              /* ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/home');
+                    },
+                    child: Text("Enter")),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/signup');
+                    },
+                    child: Text("Register"))*/
+            ],
           ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/home');
-                },
-                child: Text("Enter")),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/signup');
-                },
-                child: Text("Register"))
-          ],
         ),
       ),
     );
