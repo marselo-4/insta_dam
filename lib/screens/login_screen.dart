0cb5799ee73c2168409266bc1d80f99ffb144c4c
LujanIdn <lujancastillo2015@alumnes.ilerna.com>
2024-11-02 19:39:21 +0100

    021124_Lujan_AuthScreens_fase2

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -12,9 +12,9 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.white,
+      resizeToAvoidBottomInset: true,
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
@@ -31,8 +31,11 @@ class _LoginScreenState extends State<LoginScreen> {
                 width: 300,
                 height: 100,
               ),
+              const SizedBox(
+                height: 20,
+              ),
               const Text(
-                'Conecta a través de imágenes',
+                'Connect through images',
                 style: TextStyle(
                   color: Colors.black,
                   fontSize: 24,
@@ -42,61 +45,110 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
                 textAlign: TextAlign.center,
               ),
-              SizedBox(height: 20,),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Username',
-                  labelStyle: TextStyle(color: Colors.black, fontSize: 15),
-                  filled: true,
-                  fillColor: Colors.black12,
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(color: Colors.black, width: 3)),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(color: Colors.pink, width: 3)),
-                  prefixIcon: Icon(Icons.person),
+               const SizedBox(
+                 height: 20,
+               ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  children: [
+                    const TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Username',
+                        labelStyle:
+                            TextStyle(color: Colors.lightBlue, fontSize: 15),
+                        prefixIcon: Icon(Icons.person, color: Colors.lightBlue,),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.greenAccent, width: 3.0),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 2.0),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 50),
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: 'Password',
+                          labelStyle: const TextStyle(
+                              color: Colors.lightBlue, fontSize: 15),
+                          enabledBorder: const UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.black, width: 2.0),
+                          ),
+                          focusedBorder: const UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.greenAccent, width: 3.0),
+                          ),
+                          prefixIcon: const Icon(Icons.lock_outline_rounded , color: Colors.lightBlue,),
+                          suffixIcon: IconButton(  color: Colors.lightBlue,
+                            icon: Icon(visible
+                                ? Icons.visibility_off
+                                : Icons.visibility),
+                            onPressed: () {
+                              setState(() {
+                                visible = !visible;
+                              });
+                            },
+                          )),
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.next,
+                      obscureText: visible,
+                    ),
+                  ],
                 ),
-                keyboardType: TextInputType.text,
-                textInputAction: TextInputAction.next,
               ),
-              SizedBox(height: 50),
-              TextField(
-                decoration: InputDecoration(
-                    labelText: 'Password',
-                    labelStyle: TextStyle(color: Colors.black, fontSize: 15),
-                    filled: true,
-                    fillColor: Colors.black12,
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.black, width: 3)),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.pink, width: 3)),
-                    prefixIcon: Icon(Icons.lock_outline_rounded),
-                    suffixIcon: IconButton(
-                      icon: Icon(visible ? Icons.visibility_off : Icons.visibility),
-                      onPressed: () {
-                        setState(() {
-                        visible = !visible;
-                        });
-                      },
-                    )),
-                keyboardType: TextInputType.text,
-                textInputAction: TextInputAction.next,
-                obscureText: visible,
+              const SizedBox(
+                height: 40,
               ),
-              
-              /* ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/home');
-                    },
-                    child: Text("Enter")),
-                ElevatedButton(
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
                     onPressed: () {
                       Navigator.pushNamed(context, '/signup');
                     },
-                    child: Text("Register"))*/
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            const Color.fromARGB(255, 100, 150, 238),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 30, vertical: 10),
+                        textStyle: const TextStyle(fontSize: 18),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    child: Text(
+                      'Signup',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/home');
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            const Color.fromARGB(255, 125, 247, 131),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 60, vertical: 15),
+                        textStyle: const TextStyle(fontSize: 14),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    child: Text(
+                      'Enter',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20),
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
@@ -105,9 +157,3 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 }
 
-
-// TODO: Logo
-// TODO: Eslogan
-// TODO: Username + password
-// TODO: Enter button --> home_screen
-// TODO: Register button --> signup_screen
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,28 +1,217 @@
 import 'package:flutter/material.dart';
 
-class SignUpScreen extends StatelessWidget {
+class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  bool visible1 = true;
+  bool visible2 = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(),
-          TextField(),
-          TextField(),
-          TextField(),
-          TextField(),
-          ElevatedButton(onPressed: () { Navigator.pushNamed(context, '/home');}, child: Text("Create account")),
-          ElevatedButton(onPressed: () { Navigator.pushNamed(context, '/login');}, child: Text("Login"))
-        ],
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/img/logo.png",
+                width: 100,
+                height: 75,
+              ),
+              Image.asset(
+                "assets/img/instadam.jpg",
+                width: 150,
+                height: 60,
+              ),
+              const Text(
+                'Connect through images',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w900,
+                  fontFamily: 'roboto',
+                  letterSpacing: 1.2,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: Column(
+                  children: [
+                    const TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Name',
+                        labelStyle:
+                            TextStyle(color: Colors.lightBlue, fontSize: 15),
+                        prefixIcon: Icon(Icons.person , color: Colors.lightBlue,),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.greenAccent, width: 3.0),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 2.0),
+                        ),
+                      ),
+                      textInputAction: TextInputAction.next,
+                    ),
+                    const SizedBox(height: 20),
+                    const TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Surname',
+                        labelStyle:
+                            TextStyle(color: Colors.lightBlue, fontSize: 15),
+                        prefixIcon: Icon(Icons.person , color: Colors.lightBlue,),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.greenAccent, width: 3.0),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 2.0),
+                        ),
+                      ),
+                      textInputAction: TextInputAction.next,
+                    ),
+                    const SizedBox(height: 20),
+                    const TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Username',
+                        labelStyle:
+                            TextStyle(color: Colors.lightBlue, fontSize: 15),
+                        prefixIcon: Icon(Icons.person , color: Colors.lightBlue,),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.greenAccent, width: 3.0),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 2.0),
+                        ),
+                      ),
+                      textInputAction: TextInputAction.next,
+                    ),
+                    const SizedBox(height: 20),
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: 'Password',
+                          labelStyle: const TextStyle(
+                              color: Colors.lightBlue, fontSize: 15),
+                          enabledBorder: const UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.black, width: 2.0),
+                          ),
+                          focusedBorder: const UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.greenAccent, width: 3.0),
+                          ),
+                          prefixIcon: const Icon(Icons.lock_outline_rounded , color: Colors.lightBlue,),
+                          suffixIcon: IconButton(  color: Colors.lightBlue,
+                            icon: Icon(visible1
+                                ? Icons.visibility_off
+                                : Icons.visibility),
+                            onPressed: () {
+                              setState(() {
+                                visible1 = !visible1;
+                              });
+                            },
+                          )),
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.next,
+                      obscureText: visible1,
+                    ),
+                    const SizedBox(height: 20),
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: 'Repeat',
+                          labelStyle: const TextStyle(
+                              color: Colors.lightBlue, fontSize: 15),
+                          enabledBorder: const UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.black, width: 2.0),
+                          ),
+                          focusedBorder: const UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.greenAccent, width: 3.0),
+                          ),
+                          prefixIcon: const Icon(Icons.lock_outline_rounded , color: Colors.lightBlue,),
+                          suffixIcon: IconButton(  color: Colors.lightBlue,
+                            icon: Icon(visible2
+                                ? Icons.visibility_off
+                                : Icons.visibility),
+                            onPressed: () {
+                              setState(() {
+                                visible2 = !visible2;
+                              });
+                            },
+                          )),
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.next,
+                      obscureText: visible2,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/login');
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            const Color.fromARGB(255, 125, 247, 131),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 10),
+                        textStyle: const TextStyle(fontSize: 18),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/home');
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            const Color.fromARGB(255, 100, 150, 238),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 15),
+                        textStyle: const TextStyle(fontSize: 14),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    child: const Text(
+                      'Create account',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 20),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
 }
-
-// TODO: name, surname, usarname(id unique), password, repeat
-// TODO: Create account button
-// TODO:  Shared prefernces check id unic username
-// TODO: Login button --> login_screen
