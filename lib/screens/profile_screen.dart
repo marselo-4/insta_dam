566c5a6e5436b2741332cb69d02ede163d3f45aa
Carlos Martínez (marselo.exe) <78843770+marselo-4@users.noreply.github.com>
2024-11-15 15:01:08 +0100

    Add ViewPostWidget for displaying post details with user interaction

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,39 +1,44 @@
-  import 'dart:io';
-  import 'dart:math';
-  import 'package:insta_dam/services/list_storage_services.dart';
-  import 'package:flutter/material.dart';
-  import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:io';
+import 'dart:math';
+import 'package:insta_dam/services/list_storage_services.dart';
+import 'package:flutter/material.dart';
+import 'package:insta_dam/widgets/post_widget.dart';
+import 'package:insta_dam/widgets/view_post_widget.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
 
-  class ProfileScreen extends StatefulWidget {
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
 
-    const ProfileScreen({super.key});
+class _ProfileScreenState extends State<ProfileScreen> {
+  List<Post> posts = [];
 
-    @override
-    State<ProfileScreen> createState() => _ProfileScreenState();
-  }
-
-  class _ProfileScreenState extends State<ProfileScreen> {
-    List<String> posts = [];
-
-    @override
+  @override
   void initState() {
     super.initState();
     loadPosts();
     loadData();
   }
 
-    void loadPosts() async {
+  void loadPosts() async {
     final postList = SharedPrefList();
-    
-    final List<String>? imagePaths = await postList.getList('imageList'); 
+
+    final List<String>? imagePaths = await postList.getList('imageList');
+    final List<String>? descriptionList =
+        await postList.getList('descriptionList');
 
     if (imagePaths != null) {
       setState(() {
         posts.clear();
 
         for (int i = 0; i < imagePaths.length; i++) {
-          posts.add(imagePaths[i]);
+          posts.add(Post(
+              postImage: imagePaths[i],
+              description: descriptionList![i],
+              userName: username ?? ""));
         }
       });
     } else {
@@ -41,193 +46,220 @@
     }
   }
 
-void loadData() async {
-  final prefs = await SharedPreferences.getInstance();
-  SharedPrefList data = SharedPrefList();
-  int? userId = prefs.getInt('userId');
+  void loadData() async {
+    final prefs = await SharedPreferences.getInstance();
+    SharedPrefList data = SharedPrefList();
+    int? userId = prefs.getInt('userId');
 
-  if (userId != null) {
-    // Usa await para obtener los valores específicos de la lista según el ID del usuario
-    username = await data.getDataById('username', userId);
-    String? name = await data.getDataById('name', userId);
-    String? surname = await data.getDataById('surname', userId);
-    accName = '$name $surname';
-  } else {
-    // Si no hay un userId, toma los valores directamente de SharedPreferences
-    accName = '${prefs.getString("name")} ${prefs.getString("surname")}';
-  }
-
-  // Actualiza el estado para reflejar los cambios en la interfaz de usuario
-  setState(() {});
-}
-
-  
-    String followersNumber = Random().nextInt(1000).toString();
-    String followingNumber = Random().nextInt(100).toString();
-    String? username = "";
-    String? accName = '';
-    String? descripcion = 'Viviendo una aventura a la vez 🌍✨';    
-
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-          body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Column(
-              children: [
-                Container(
-                    width: MediaQuery.of(context).size.width,
-                    child: Align(
-                      alignment: const FractionalOffset(.05, 2),
-                      child: Text(
-                      username!,
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    )
-                  ),
-            
-            //foto + seguidores, seguidos...
-            Container(
-                width: MediaQuery.of(context).size.width,
-                height: 120,
-                padding: const EdgeInsets.only(left: 8, right: 8),
-                child:  Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Align(
-                      alignment: FractionalOffset(.2, .5),
-                      child: CircleAvatar(backgroundImage: AssetImage('assets/img/avatar.jpg'),
-                      radius: 45,)
-                    ),
-                    
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(posts.length.toString(), style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-                        const Text("pubicaciones")
-                      ],
-                    ),
-            
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(followersNumber, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-                        const Text("seguidores")
-                      ],  
-                    ),
-            
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(followingNumber, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-                        const Text("siguiendo")
-                      ],
-                    )
-                  ],
-                )
-            
-                
-              ),
-            // descripcion
-            Center(
-              
-              child: Container(
-                padding: const EdgeInsets.only(left: 8, right: 8),
-                width: MediaQuery.of(context).size.width,
-                constraints: BoxConstraints(maxHeight: 150),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    //Nombre persona
-                    Text(accName!, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
-                  
-                  //descripcion
-                  Text(descripcion!, maxLines: 5, overflow: TextOverflow.ellipsis,)
-                  ],
-                  
-                ),
-              ),
-            ),
-            //editar + compartir
-            Center(
-              child: Container(
-                padding: const EdgeInsets.only(left: 8, right: 8),
-                width: MediaQuery.of(context).size.width,
-                height: 75,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
-                  children: [
-                    Expanded(
-                      child: ElevatedButton(
-                        onPressed: () {
-                        },
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
-                            side: const BorderSide(color: Colors.black, width: .5), 
-                          ),
-                          padding: const EdgeInsets.all(8), 
-                        ),
-                        child: const Text(
-                          'Editar',
-                          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black, fontSize: 16), 
-                        ),
-                      ),
-                    ),
-            
-                    const SizedBox(width: 8), 
-            
-                    Expanded(
-                      child: ElevatedButton(
-                        onPressed: () {
-                        },
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
-                            side: const BorderSide(color: Colors.black, width: .5),
-                          ),
-                          padding: const EdgeInsets.all(8),
-                        ),
-                        child: const Text(
-                          'Compartir',
-                          style: TextStyle(color: Colors.black, fontSize: 16), 
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-            
-              ),
-            ),
-            //posts
-            Expanded(
-              child: GridView.builder(
-                
-                itemCount: posts.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3), 
-                itemBuilder: (context, index){
-                  return Padding(
-                    padding: const EdgeInsets.all(2),
-                    child: Container(
-                      height: 50,
-                      width: 50,
-                      child: Image.file(File(posts[index]), fit: BoxFit.cover,)
-                    ),
-                  );
-                }
-                ),
-            ),
-            
-              ],
-            ),
-          )
-        
-      );
+    if (userId != null) {
+      // Usa await para obtener los valores específicos de la lista según el ID del usuario
+      username = await data.getDataById('username', userId);
+      String? name = await data.getDataById('name', userId);
+      String? surname = await data.getDataById('surname', userId);
+      accName = '$name $surname';
+    } else {
+      // Si no hay un userId, toma los valores directamente de SharedPreferences
+      accName = '${prefs.getString("name")} ${prefs.getString("surname")}';
     }
+
+    // Actualiza el estado para reflejar los cambios en la interfaz de usuario
+    setState(() {});
   }
+
+  String followersNumber = Random().nextInt(1000).toString();
+  String followingNumber = Random().nextInt(100).toString();
+  String? username = "";
+  String? accName = '';
+  String? descripcion = 'Viviendo una aventura a la vez 🌍✨';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Column(
+        children: [
+          Container(
+              width: MediaQuery.of(context).size.width,
+              child: Align(
+                alignment: const FractionalOffset(.05, 2),
+                child: Text(
+                  username!,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              )),
+
+          //foto + seguidores, seguidos...
+          Container(
+              width: MediaQuery.of(context).size.width,
+              height: 120,
+              padding: const EdgeInsets.only(left: 8, right: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Align(
+                      alignment: FractionalOffset(.2, .5),
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage('assets/img/avatar.jpg'),
+                        radius: 45,
+                      )),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        posts.length.toString(),
+                        style: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      const Text("pubicaciones")
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        followersNumber,
+                        style: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      const Text("seguidores")
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        followingNumber,
+                        style: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      const Text("siguiendo")
+                    ],
+                  )
+                ],
+              )),
+          // descripcion
+          Center(
+            child: Container(
+              padding: const EdgeInsets.only(left: 8, right: 8),
+              width: MediaQuery.of(context).size.width,
+              constraints: BoxConstraints(maxHeight: 150),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //Nombre persona
+                  Text(
+                    accName!,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                  ),
+
+                  //descripcion
+                  Text(
+                    descripcion!,
+                    maxLines: 5,
+                    overflow: TextOverflow.ellipsis,
+                  )
+                ],
+              ),
+            ),
+          ),
+          //editar + compartir
+          Center(
+            child: Container(
+              padding: const EdgeInsets.only(left: 8, right: 8),
+              width: MediaQuery.of(context).size.width,
+              height: 75,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          side:
+                              const BorderSide(color: Colors.black, width: .5),
+                        ),
+                        padding: const EdgeInsets.all(8),
+                      ),
+                      child: const Text(
+                        'Editar',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 16),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          side:
+                              const BorderSide(color: Colors.black, width: .5),
+                        ),
+                        padding: const EdgeInsets.all(8),
+                      ),
+                      child: const Text(
+                        'Compartir',
+                        style: TextStyle(color: Colors.black, fontSize: 16),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+                itemCount: posts.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3),
+                itemBuilder: (context, index) {
+                  return Padding(
+                      padding: const EdgeInsets.all(2),
+                      child: GestureDetector(
+                          onTap: () {
+                            print(posts[index]);
+                            //TODO: HACER QUE AL HACER CLICK EN UNA IMAGEN SE ABRA LA PANTALLA DE VIEW POST
+                            showDialog(
+                              context: context,
+                              builder: (context) => Dialog(
+                                child: ConstrainedBox(
+                                    constraints: BoxConstraints(
+                                      maxHeight:
+                                          MediaQuery.of(context).size.height *
+                                              0.6,
+                                      maxWidth:
+                                          MediaQuery.of(context).size.width *
+                                              0.8,
+                                    ),
+                                    child: ViewPostWidget(
+                                        "$username: ${posts[index].description} ",
+                                        username ?? "",
+                                        posts[index].postImage,
+                                        posts[index].isLiked)),
+                              ),
+                            );
+                          },
+                          child: Image.file(
+                            File(posts[index].postImage),
+                            fit: BoxFit.cover,
+                          )));
+                }),
+          ),
+        ],
+      ),
+    ));
+  }
+}
---
+++ lib/widgets/view_post_widget.dart
@@ -0,0 +1,96 @@
+import 'dart:io';
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+Widget ViewPostWidget(String descripcion, String userName, String postImage, bool isLiked) {
+  return Card(
+            margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+            child: SizedBox(
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        const CircleAvatar(
+                          radius: 20,
+                          backgroundImage: AssetImage('assets/img/avatar.jpg'),
+                        ),
+                        const SizedBox(width: 10),
+                        Text(
+                          userName,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Container(
+                      width: double.infinity,
+                      height: 300,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: FileImage(File(postImage)),
+                          fit: BoxFit.cover,
+                        ),
+                        color: Colors.grey[300],
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(
+                            isLiked
+                                ? Icons.favorite
+                                : Icons.favorite_border,
+                            color: isLiked ? Colors.red : null,
+                          ),
+                          onPressed: () {
+                            isLiked = !isLiked;
+                                                      },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.chat_bubble_rounded),
+                          onPressed: () {
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.send_rounded),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Liked by ${userName} and ${Random().nextInt(100)} others',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        Text(descripcion, style: const TextStyle(fontWeight: FontWeight.w700),),
+                        const SizedBox(height: 5),
+                        Text(
+                          Random().nextInt(23) + 1 > 1
+                              ? '${Random().nextInt(24)} hours ago'
+                              : '${Random().nextInt(24)} hour ago',
+                          style: const TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+  }
+
